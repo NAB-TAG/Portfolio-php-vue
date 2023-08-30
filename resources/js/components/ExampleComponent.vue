723faf24d0ae86fb93e14971b73939605154e90b
Nando Agustin Bravo <bravonandoagustin@gmail.com>
2023-08-30 11:03:26 -0300

    My first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>hello world, I'am nando</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log("asdfasdf")
+            console.log('nando.')
+        }
+    }
+</script>
+
+<style>
+    body{
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        background-color: red;
+    }
+</style>
